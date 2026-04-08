e258b5c339ab338cf2256d1911690d1cd902875a
GqbeBolt <95443729+GqbeBolt@users.noreply.github.com>
2026-04-08 14:19:01 -0700

    fixed movement (hopefully)

--- CMPM125 Assignment 1/Assets/Scripts/VehicleController.cs
+++ CMPM125 Assignment 1/Assets/Scripts/VehicleController.cs
@@ -6,7 +6,7 @@ public class VehicleController : MonoBehaviour
 {
 
     private float desired_accelerationX;
-    private float desired_accelerationY;
+    private float desired_accelerationZ;
     public float power;
     public float turnSpeed;
 
@@ -28,7 +28,7 @@ public class VehicleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().AddRelativeForce(new Vector3(desired_accelerationX * power, 0, desired_accelerationY * power));
+        GetComponent<Rigidbody>().AddRelativeForce(new Vector3(desired_accelerationX * power, 0, desired_accelerationZ * power));
         float dx = (Mouse.current.position.x.value - Screen.width / 2) / 200;
         if (Mathf.Abs(dx) > 0.01f)
         {
@@ -38,13 +38,15 @@ public class VehicleController : MonoBehaviour
         timeLabel.text = string.Format("Current Lap Time: {0:F2} seconds", (Time.time - startTime));
         lapLabel.text = string.Format("Laps Done: {0}", laps);
         //timeLabel.text = "Current Lap Time: " + Mathf.Round((Time.time - startTime) * 100) / 100 + " seconds";
+        //Debug.Log("forward: " + desired_accelerationX + " | Side: " + desired_accelerationZ);
     }
 
     void OnMove(InputValue action)
     {
         Vector2 movement = action.Get<Vector2>();
-        desired_accelerationY = -movement.x;
+        desired_accelerationZ = -movement.x;
         desired_accelerationX = movement.y;
+       
     }
 
     public void resetTime()
