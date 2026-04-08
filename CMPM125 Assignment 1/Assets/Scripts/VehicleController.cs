50931a592a7439b5b541eff0dc1e8cc216f3e3fb
Gabriel Rybolt <95443729+GqbeBolt@users.noreply.github.com>
2026-04-08 10:24:11 -0700

    done w/o terrain

--- CMPM125 Assignment 1/Assets/Scripts/VehicleController.cs
+++ CMPM125 Assignment 1/Assets/Scripts/VehicleController.cs
@@ -5,36 +5,55 @@ using TMPro;
 public class VehicleController : MonoBehaviour
 {
 
-    private float desired_acceleration;
+    private float desired_accelerationX;
+    private float desired_accelerationY;
     public float power;
+    public float turnSpeed;
 
     public WaypointController next;
 
     public TextMeshProUGUI timeLabel;
+    public TextMeshProUGUI lapLabel;
+    int laps = 0;
     float startTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startTime = Time.time;
+        next.left.materials[0].color = Color.red;
+        next.right.materials[0].color = Color.red;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().AddRelativeForce(new Vector3(desired_acceleration * power, 0, 0));
+        GetComponent<Rigidbody>().AddRelativeForce(new Vector3(desired_accelerationX * power, 0, desired_accelerationY * power));
         float dx = (Mouse.current.position.x.value - Screen.width / 2) / 200;
         if (Mathf.Abs(dx) > 0.01f)
         {
-            transform.Rotate(0, dx, 0);
+            transform.Rotate(0, dx/20 * turnSpeed, 0);
         }
 
-        timeLabel.text = (Time.time - startTime).ToString() + " seconds";
+        timeLabel.text = string.Format("Current Lap Time: {0:F2} seconds", (Time.time - startTime));
+        lapLabel.text = string.Format("Laps Done: {0}", laps);
+        //timeLabel.text = "Current Lap Time: " + Mathf.Round((Time.time - startTime) * 100) / 100 + " seconds";
     }
 
     void OnMove(InputValue action)
     {
         Vector2 movement = action.Get<Vector2>();
-        desired_acceleration = movement.y;
+        desired_accelerationY = -movement.x;
+        desired_accelerationX = movement.y;
+    }
+
+    public void resetTime()
+    {
+        startTime = Time.time;    
+    }
+
+    public void lapDone()
+    {
+        laps++;
     }
 }
--- CMPM125 Assignment 1/Assets/Scripts/WaypointController.cs
+++ CMPM125 Assignment 1/Assets/Scripts/WaypointController.cs
@@ -6,26 +6,28 @@ public class WaypointController : MonoBehaviour
 
 
     public WaypointController next;
+
+    public MeshRenderer left, right;
+
+    public bool lastPoint = false;
     
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         var vehicle = other.gameObject.GetComponent<VehicleController>();
         if (vehicle != null)
         {
-            Debug.Log("hey");
+            next.left.materials[0].color = Color.red;
+            next.right.materials[0].color = Color.red;
+
+            left.materials[0].color = Color.white;
+            right.materials[0].color = Color.white;
+
+            if (lastPoint)
+            {
+                vehicle.resetTime();
+                vehicle.lapDone();
+            }
         }
     }
 }
